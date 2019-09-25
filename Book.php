a7f50e87961cea6f8227bd0fa477a2fadfe70ee7
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2019-09-25 09:33:54 +0200

    Fonctions à implémenter

---
+++ Book.php
@@ -0,0 +1,64 @@
+<?php
+
+
+class Book
+{
+    private $title;
+    private $author;
+    private $editor;
+    private $pageNb;
+
+    public function __construct($author, $editor, $pageNb, $title)
+    {
+        $this->author = $author;
+        $this->editor = $editor;
+        $this->pageNb = $pageNb;
+        $this->title = $title;
+    }
+
+    // GETTERS
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function getEditor()
+    {
+        return $this->editor;
+    }
+
+    public function getPageNb()
+    {
+        return $this->pageNb;
+    }
+
+    // SETTERS
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    public function setEditor($editor)
+    {
+        $this->editor = $editor;
+    }
+
+    public function setPageNb($pageNb)
+    {
+        $this->pageNb = $pageNb;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    // MISC.
+
+    public function show() {}
+}
---
+++ Library.php
@@ -0,0 +1,79 @@
+<?php
+
+
+class Library
+{
+    const MAX_BOOKS = 5;
+
+    private $name;
+    private $adress;
+    private $max;
+    private $books = array(self::MAX_BOOKS);
+
+    public function __construct($name, $adress, $max)
+    {
+        $this->name = $name;
+        $this->adress = $adress;
+        $this->max = $max;
+    }
+
+    // GETTERS
+    public function getAdress()
+    {
+        return $this->adress;
+    }
+
+    public function getBooks()
+    {
+        return $this->books;
+    }
+
+    public function getMax()
+    {
+        return $this->max;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    // SETTERS
+    public function setAdress($adress)
+    {
+        $this->adress = $adress;
+    }
+
+    public function setBooks($books)
+    {
+        $this->books = $books;
+    }
+
+    public function setMax($max)
+    {
+        $this->max = $max;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    // MISC.
+
+    public function showBooks() {
+        foreach ($this->books as &$book ) {
+            $book.show();
+        }
+    }
+
+    public function addBook () {}
+
+    public function removeBook () {}
+
+    public function deleteDuplicates () {}
+
+    public function showCommonBooks (Library $otherLibrary) {}
+
+    public function sortByAuthor () {}
+}
