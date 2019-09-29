a26e6d93a37050f4c5c30ce5ba8908f2b89bc6a3
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2019-09-29 21:50:07 +0200

    style

--- index.php
+++ index.php
@@ -8,34 +8,36 @@
 <body>
     <div class="bookForm">
         <form action="createBook.php" method="POST">
-            <label for="title">Titre</label>
-            <input name="title" placeholder="Entrez le titre de livre" type="text">
+            <h1 class="bookTextColor"> Book </h1>
+            <label class="bookTextColor" for="title">Titre</label>
+            <input class="breath" name="title" placeholder="Entrez le titre de livre" type="text">
 
-            <label for="author">Auteur</label>
-            <input name="author" placeholder="Entrez l'auteur du livre" type="text">
+            <label class="bookTextColor" for="author">Auteur</label>
+            <input class="breath" name="author" placeholder="Entrez l'auteur du livre" type="text">
 
-            <label for="editor">Editeur</label>
-            <input name="editor" placeholder="Entrez l'éditeur du livre" type="text">
+            <label class="bookTextColor" for="editor">Editeur</label>
+            <input class="breath" name="editor" placeholder="Entrez l'éditeur du livre" type="text">
 
-            <label for="pages">Nombre de pages</label>
-            <input name="pages" placeholder="Entrez le nombre de pages du livre" type="text">
+            <label class="bookTextColor" for="pages">Nombre de pages</label>
+            <input class="breath" name="pages" placeholder="Entrez le nombre de pages du livre" type="text">
 
-            <input name="action" type="submit" value="Créer">
+            <input class="bookTextColor" name="action" type="submit" value="Créer">
         </form>
     </div>
 
     <div class="libForm">
+        <h1 class="libTextColor"> Library </h1>
         <form action="createLibrary.php" method="POST">
-            <label for="libName">Nom</label>
+            <label class="libTextColor" for="libName">Nom</label>
             <input name="libName" placeholder="Bibliothèque nationale de France" type="text">
 
-            <label for="libAddress">Adresse</label>
+            <label class="libTextColor" for="libAddress">Adresse</label>
             <input name="libAddress" placeholder="Quai François Mauriax, 75706 Paris" type="text">
 
-            <label for="libMax">Nb. de livres max</label>
+            <label class="libTextColor" for="libMax">Nb. de livres max</label>
             <input name="libMax" placeholder="5" type="text">
 
-            <input name="action" type="submit" value="Créer">
+            <input class="libTextColor" name="action" type="submit" value="Créer">
         </form>
     </div>
 
